d17da58d676c6f140772da629ea0f58571dd976d
Javier Alejandro Hernandez Santana <hernandezjale@bancoavvillas.com.co>
2020-02-26 16:08:24 -0500

    Creacion proyecto wssecurity WFC

---
+++ clientews/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clientews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cliente=  new clientews.manejoidentidad01.WSBA_ManejoIdentidad_autenticar_FMClient();
+            var respuesta = cliente.autenticar(new manejoidentidad01.autenticar() { oe_entrada = new manejoidentidad01.oe_Autenticacion() { strUsuario = "1032396905", strContrasena = "PWd12345" } });
+            Console.WriteLine("Respuesta WS");
+            Console.WriteLine(" " + respuesta.os_salida.@return);
+
+            Console.Write("\n\n\tPress any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
